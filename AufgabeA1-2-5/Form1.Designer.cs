1c1675aad50025348f0600826f192dba5a58c7a4
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2020-10-26 20:54:18 +0100

    Done Aufgabe A1-2-5

---
+++ AufgabeA1-2-5/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace AufgabeA1_2_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdBit1 = new System.Windows.Forms.Button();
+            this.CmdBit2 = new System.Windows.Forms.Button();
+            this.CmdBit3 = new System.Windows.Forms.Button();
+            this.CmdBit4 = new System.Windows.Forms.Button();
+            this.CmdBit8 = new System.Windows.Forms.Button();
+            this.CmdBit7 = new System.Windows.Forms.Button();
+            this.CmdBit6 = new System.Windows.Forms.Button();
+            this.CmdBit5 = new System.Windows.Forms.Button();
+            this.CmdCalc = new System.Windows.Forms.Button();
+            this.CmdReset = new System.Windows.Forms.Button();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdBit1
+            // 
+            this.CmdBit1.Location = new System.Drawing.Point(264, 51);
+            this.CmdBit1.Name = "CmdBit1";
+            this.CmdBit1.Size = new System.Drawing.Size(30, 30);
+            this.CmdBit1.TabIndex = 0;
+            this.CmdBit1.Text = "0";
+            this.CmdBit1.UseVisualStyleBackColor = true;
+            this.CmdBit1.Click += new System.EventHandler(this.CmdBit1_Click);
+            // 
+            // CmdBit2
+            // 
+            this.CmdBit2.Location = new System.Drawing.Point(228, 51);
+            this.CmdBit2.Name = "CmdBit2";
+            this.CmdBit2.Size = new System.Drawing.Size(30, 30);
+            this.CmdBit2.TabIndex = 1;
+            this.CmdBit2.Text = "0";
+            this.CmdBit2.UseVisualStyleBackColor = true;
+            this.CmdBit2.Click += new System.EventHandler(this.CmdBit2_Click);
+            // 
+            // CmdBit3
+            // 
+            this.CmdBit3.Location = new System.Drawing.Point(192, 51);
+            this.CmdBit3.Name = "CmdBit3";
+            this.CmdBit3.Size = new System.Drawing.Size(30, 30);
+            this.CmdBit3.TabIndex = 2;
+            this.CmdBit3.Text = "0";
+            this.CmdBit3.UseVisualStyleBackColor = true;
+            this.CmdBit3.Click += new System.EventHandler(this.CmdBit3_Click);
+            // 
+            // CmdBit4
+            // 
+            this.CmdBit4.Location = new System.Drawing.Point(156, 51);
+            this.CmdBit4.Name = "CmdBit4";
+            this.CmdBit4.Size = new System.Drawing.Size(30, 30);
+            this.CmdBit4.TabIndex = 3;
+            this.CmdBit4.Text = "0";
+            this.CmdBit4.UseVisualStyleBackColor = true;
+            this.CmdBit4.Click += new System.EventHandler(this.CmdBit4_Click);
+            // 
+            // CmdBit8
+            // 
+            this.CmdBit8.Location = new System.Drawing.Point(12, 51);
+            this.CmdBit8.Name = "CmdBit8";
+            this.CmdBit8.Size = new System.Drawing.Size(30, 30);
+            this.CmdBit8.TabIndex = 7;
+            this.CmdBit8.Text = "0";
+            this.CmdBit8.UseVisualStyleBackColor = true;
+            this.CmdBit8.Click += new System.EventHandler(this.CmdBit8_Click);
+            // 
+            // CmdBit7
+            // 
+            this.CmdBit7.Location = new System.Drawing.Point(48, 51);
+            this.CmdBit7.Name = "CmdBit7";
+            this.CmdBit7.Size = new System.Drawing.Size(30, 30);
+            this.CmdBit7.TabIndex = 6;
+            this.CmdBit7.Text = "0";
+            this.CmdBit7.UseVisualStyleBackColor = true;
+            this.CmdBit7.Click += new System.EventHandler(this.CmdBit7_Click);
+            // 
+            // CmdBit6
+            // 
+            this.CmdBit6.Location = new System.Drawing.Point(84, 51);
+            this.CmdBit6.Name = "CmdBit6";
+            this.CmdBit6.Size = new System.Drawing.Size(30, 30);
+            this.CmdBit6.TabIndex = 5;
+            this.CmdBit6.Text = "0";
+            this.CmdBit6.UseVisualStyleBackColor = true;
+            this.CmdBit6.Click += new System.EventHandler(this.CmdBit6_Click);
+            // 
+            // CmdBit5
+            // 
+            this.CmdBit5.Location = new System.Drawing.Point(120, 51);
+            this.CmdBit5.Name = "CmdBit5";
+            this.CmdBit5.Size = new System.Drawing.Size(30, 30);
+            this.CmdBit5.TabIndex = 4;
+            this.CmdBit5.Text = "0";
+            this.CmdBit5.UseVisualStyleBackColor = true;
+            this.CmdBit5.Click += new System.EventHandler(this.CmdBit5_Click);
+            // 
+            // CmdCalc
+            // 
+            this.CmdCalc.Location = new System.Drawing.Point(12, 115);
+            this.CmdCalc.Name = "CmdCalc";
+            this.CmdCalc.Size = new System.Drawing.Size(284, 23);
+            this.CmdCalc.TabIndex = 8;
+            this.CmdCalc.Text = "Berechne Dezimalwert";
+            this.CmdCalc.UseVisualStyleBackColor = true;
+            this.CmdCalc.Click += new System.EventHandler(this.CmdCalc_Click);
+            // 
+            // CmdReset
+            // 
+            this.CmdReset.Location = new System.Drawing.Point(12, 144);
+            this.CmdReset.Name = "CmdReset";
+            this.CmdReset.Size = new System.Drawing.Size(282, 23);
+            this.CmdReset.TabIndex = 9;
+            this.CmdReset.Text = "Setze zurück";
+            this.CmdReset.UseVisualStyleBackColor = true;
+            this.CmdReset.Click += new System.EventHandler(this.CmdReset_Click);
+            // 
+            // LblResult
+            // 
+            this.LblResult.AutoSize = true;
+            this.LblResult.Location = new System.Drawing.Point(12, 207);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(84, 13);
+            this.LblResult.TabIndex = 10;
+            this.LblResult.Text = "Wert in Dezimal:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 243);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.CmdReset);
+            this.Controls.Add(this.CmdCalc);
+            this.Controls.Add(this.CmdBit8);
+            this.Controls.Add(this.CmdBit7);
+            this.Controls.Add(this.CmdBit6);
+            this.Controls.Add(this.CmdBit5);
+            this.Controls.Add(this.CmdBit4);
+            this.Controls.Add(this.CmdBit3);
+            this.Controls.Add(this.CmdBit2);
+            this.Controls.Add(this.CmdBit1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdBit1;
+        private System.Windows.Forms.Button CmdBit2;
+        private System.Windows.Forms.Button CmdBit3;
+        private System.Windows.Forms.Button CmdBit4;
+        private System.Windows.Forms.Button CmdBit8;
+        private System.Windows.Forms.Button CmdBit7;
+        private System.Windows.Forms.Button CmdBit6;
+        private System.Windows.Forms.Button CmdBit5;
+        private System.Windows.Forms.Button CmdCalc;
+        private System.Windows.Forms.Button CmdReset;
+        private System.Windows.Forms.Label LblResult;
+    }
+}
+
---
+++ AufgabeA1-2-5/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AufgabeA1_2_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdBit1_Click(object sender, EventArgs e)
+        {
+            CmdBit1.Text = flip(CmdBit1.Text);
+        }
+
+        private void CmdBit2_Click(object sender, EventArgs e)
+        {
+            CmdBit2.Text = flip(CmdBit2.Text);
+        }
+
+        private void CmdBit3_Click(object sender, EventArgs e)
+        {
+            CmdBit3.Text = flip(CmdBit3.Text);
+        }
+
+        private void CmdBit4_Click(object sender, EventArgs e)
+        {
+            CmdBit4.Text = flip(CmdBit4.Text);
+        }
+
+        private void CmdBit5_Click(object sender, EventArgs e)
+        {
+            CmdBit5.Text = flip(CmdBit5.Text);
+        }
+
+        private void CmdBit6_Click(object sender, EventArgs e)
+        {
+            CmdBit6.Text = flip(CmdBit6.Text);
+        }
+
+        private void CmdBit7_Click(object sender, EventArgs e)
+        {
+            CmdBit7.Text = flip(CmdBit7.Text);
+        }
+
+        private void CmdBit8_Click(object sender, EventArgs e)
+        {
+            CmdBit8.Text = flip(CmdBit8.Text);
+        }
+
+        private string flip(string current_int)
+        {
+            if (current_int == "0")
+            {
+                return "1";
+            }
+
+            return "0";
+        }
+
+        private void CmdCalc_Click(object sender, EventArgs e)
+        {
+            string bits = CmdBit8.Text + CmdBit7.Text + CmdBit6.Text + CmdBit5.Text + CmdBit4.Text + CmdBit3.Text + CmdBit2.Text + CmdBit1.Text;
+            LblResult.Text = "Wert in Dezimal: " + Convert.ToInt16(bits, 2).ToString();
+        }
+
+        private void CmdReset_Click(object sender, EventArgs e)
+        {
+            CmdBit1.Text = "0";
+            CmdBit2.Text = "0";
+            CmdBit3.Text = "0";
+            CmdBit4.Text = "0";
+            CmdBit5.Text = "0";
+            CmdBit6.Text = "0";
+            CmdBit7.Text = "0";
+            CmdBit8.Text = "0";
+        }
+    }
+}
