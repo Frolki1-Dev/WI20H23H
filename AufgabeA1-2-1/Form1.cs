a52a55db51e6cf3fc148074c1cff99c2156481fd
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2020-10-26 20:05:37 +0100

    Done Aufgabe A1-2-1

---
+++ AufgabeA1-2-1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AufgabeA1_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdYellow_Click(object sender, EventArgs e)
+        {
+            LblYellow.Text = "Gelbe Hintergrundfarbe";
+            LblYellow.BackColor = Color.Yellow;
+        }
+
+        private void CmdRed_Click(object sender, EventArgs e)
+        {
+            LblRed.Text = "Rote Hintergundfarbe";
+            LblRed.BackColor = Color.Red;
+            LblRed.ForeColor = Color.White;
+        }
+
+        private void CmdGreen_Click(object sender, EventArgs e)
+        {
+            LblGreen.Text = "Grüne Hintergundfarbe";
+            LblGreen.BackColor = Color.Green;
+            LblGreen.ForeColor = Color.White;
+        }
+
+        private void CmdBlue_Click(object sender, EventArgs e)
+        {
+            LblBlue.Text = "Blaue Hintergundfarbe";
+            LblBlue.BackColor = Color.Blue;
+            LblBlue.ForeColor = Color.White;
+        }
+    }
+}
