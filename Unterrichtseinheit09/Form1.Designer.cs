67a11f428c72c5969e50208f0bf354aa3c077c09
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2021-02-16 19:47:56 +0100

    Added listbox

---
+++ Unterrichtseinheit09/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Unterrichtseinheit09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LstData = new System.Windows.Forms.ListBox();
+            this.CmdShowOneElement = new System.Windows.Forms.Button();
+            this.CmdShowAllElements = new System.Windows.Forms.Button();
+            this.CmdShowSelectedElement = new System.Windows.Forms.Button();
+            this.CmdShowAllSelectedElements = new System.Windows.Forms.Button();
+            this.CmdShowAllElementsWithForEach = new System.Windows.Forms.Button();
+            this.TxtData = new System.Windows.Forms.TextBox();
+            this.TxtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Die Listbox";
+            // 
+            // LstData
+            // 
+            this.LstData.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LstData.FormattingEnabled = true;
+            this.LstData.ItemHeight = 25;
+            this.LstData.Location = new System.Drawing.Point(19, 82);
+            this.LstData.Name = "LstData";
+            this.LstData.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.LstData.Size = new System.Drawing.Size(271, 254);
+            this.LstData.TabIndex = 1;
+            // 
+            // CmdShowOneElement
+            // 
+            this.CmdShowOneElement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmdShowOneElement.Location = new System.Drawing.Point(315, 82);
+            this.CmdShowOneElement.Name = "CmdShowOneElement";
+            this.CmdShowOneElement.Size = new System.Drawing.Size(377, 34);
+            this.CmdShowOneElement.TabIndex = 2;
+            this.CmdShowOneElement.Text = "Zeige ein Element >>";
+            this.CmdShowOneElement.UseVisualStyleBackColor = true;
+            this.CmdShowOneElement.Click += new System.EventHandler(this.CmdShowOneElement_Click);
+            // 
+            // CmdShowAllElements
+            // 
+            this.CmdShowAllElements.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmdShowAllElements.Location = new System.Drawing.Point(315, 136);
+            this.CmdShowAllElements.Name = "CmdShowAllElements";
+            this.CmdShowAllElements.Size = new System.Drawing.Size(377, 34);
+            this.CmdShowAllElements.TabIndex = 3;
+            this.CmdShowAllElements.Text = "Zeige alle Elemente >>";
+            this.CmdShowAllElements.UseVisualStyleBackColor = true;
+            this.CmdShowAllElements.Click += new System.EventHandler(this.CmdShowAllElements_Click);
+            // 
+            // CmdShowSelectedElement
+            // 
+            this.CmdShowSelectedElement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmdShowSelectedElement.Location = new System.Drawing.Point(315, 190);
+            this.CmdShowSelectedElement.Name = "CmdShowSelectedElement";
+            this.CmdShowSelectedElement.Size = new System.Drawing.Size(377, 34);
+            this.CmdShowSelectedElement.TabIndex = 4;
+            this.CmdShowSelectedElement.Text = "Zeige selektiertes Element >>";
+            this.CmdShowSelectedElement.UseVisualStyleBackColor = true;
+            this.CmdShowSelectedElement.Click += new System.EventHandler(this.CmdShowSelectedElement_Click);
+            // 
+            // CmdShowAllSelectedElements
+            // 
+            this.CmdShowAllSelectedElements.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmdShowAllSelectedElements.Location = new System.Drawing.Point(315, 244);
+            this.CmdShowAllSelectedElements.Name = "CmdShowAllSelectedElements";
+            this.CmdShowAllSelectedElements.Size = new System.Drawing.Size(377, 34);
+            this.CmdShowAllSelectedElements.TabIndex = 5;
+            this.CmdShowAllSelectedElements.Text = "Zeige selektierte Elemente >>";
+            this.CmdShowAllSelectedElements.UseVisualStyleBackColor = true;
+            this.CmdShowAllSelectedElements.Click += new System.EventHandler(this.CmdShowAllSelectedElements_Click);
+            // 
+            // CmdShowAllElementsWithForEach
+            // 
+            this.CmdShowAllElementsWithForEach.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmdShowAllElementsWithForEach.Location = new System.Drawing.Point(315, 299);
+            this.CmdShowAllElementsWithForEach.Name = "CmdShowAllElementsWithForEach";
+            this.CmdShowAllElementsWithForEach.Size = new System.Drawing.Size(377, 34);
+            this.CmdShowAllElementsWithForEach.TabIndex = 6;
+            this.CmdShowAllElementsWithForEach.Text = "Zeige alle Elemente mit foreach >>";
+            this.CmdShowAllElementsWithForEach.UseVisualStyleBackColor = true;
+            this.CmdShowAllElementsWithForEach.Click += new System.EventHandler(this.CmdShowAllElementsWithForEach_Click);
+            // 
+            // TxtData
+            // 
+            this.TxtData.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.TxtData.Location = new System.Drawing.Point(733, 82);
+            this.TxtData.Multiline = true;
+            this.TxtData.Name = "TxtData";
+            this.TxtData.Size = new System.Drawing.Size(275, 251);
+            this.TxtData.TabIndex = 7;
+            // 
+            // TxtInput
+            // 
+            this.TxtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtInput.Location = new System.Drawing.Point(315, 40);
+            this.TxtInput.Name = "TxtInput";
+            this.TxtInput.Size = new System.Drawing.Size(377, 29);
+            this.TxtInput.TabIndex = 8;
+            this.TxtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtInput_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 369);
+            this.Controls.Add(this.TxtInput);
+            this.Controls.Add(this.TxtData);
+            this.Controls.Add(this.CmdShowAllElementsWithForEach);
+            this.Controls.Add(this.CmdShowAllSelectedElements);
+            this.Controls.Add(this.CmdShowSelectedElement);
+            this.Controls.Add(this.CmdShowAllElements);
+            this.Controls.Add(this.CmdShowOneElement);
+            this.Controls.Add(this.LstData);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Listbox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox LstData;
+        private System.Windows.Forms.Button CmdShowOneElement;
+        private System.Windows.Forms.Button CmdShowAllElements;
+        private System.Windows.Forms.Button CmdShowSelectedElement;
+        private System.Windows.Forms.Button CmdShowAllSelectedElements;
+        private System.Windows.Forms.Button CmdShowAllElementsWithForEach;
+        private System.Windows.Forms.TextBox TxtData;
+        private System.Windows.Forms.TextBox TxtInput;
+    }
+}
+
---
+++ Unterrichtseinheit09/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unterrichtseinheit09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LstData.Items.Add("Apfel");
+            LstData.Items.Add("Banane");
+            LstData.Items.Add("Orange");
+            LstData.Items.Add("Kiwi");
+            LstData.Items.Add("Limette");
+            LstData.Items.Add("Zitrone");
+        }
+
+        private void CmdShowOneElement_Click(object sender, EventArgs e)
+        {
+            // SEHR WICHTIG
+            // Indexes fangen mit 0 an und nicht 1!
+            int count = LstData.Items.Count;
+
+            TxtData.Text = LstData.Items[count - 1].ToString();
+        }
+
+        private void CmdShowAllElements_Click(object sender, EventArgs e)
+        {
+            TxtData.Text = "";
+
+            // Count the items
+            int count = LstData.Items.Count;
+
+            // Loop
+            for(int i = 0; i < count; i++)
+            {
+                // Output
+                TxtData.Text += LstData.Items[i].ToString() + "\r\n";
+            }
+        }
+
+        private void CmdShowAllElementsWithForEach_Click(object sender, EventArgs e)
+        {
+            TxtData.Text = "";
+
+            foreach(string value in LstData.Items)
+            {
+                TxtData.Text += value + "\r\n";
+            }
+        }
+
+        private void CmdShowSelectedElement_Click(object sender, EventArgs e)
+        {
+            TxtData.Text = "";
+
+            if(LstData.SelectedIndex < 0)
+            {
+                // No selected item
+                MessageBox.Show("Kein Element ausgewählt!");
+                return;
+            }
+
+            TxtData.Text = LstData.SelectedIndex + " : " + LstData.SelectedItem;
+        }
+
+        private void CmdShowAllSelectedElements_Click(object sender, EventArgs e)
+        {
+            TxtData.Text = "";
+
+            /*foreach(string value in LstData.SelectedItems)
+            {
+                TxtData.Text += value + "\r\n";
+            }*/
+
+            int count = LstData.SelectedItems.Count;
+
+            if(count < 1)
+            {
+                MessageBox.Show("Kein Element ausgewählt!", "Nice :)", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            for(int i = 0; i < count; i++)
+            {
+                TxtData.Text += LstData.SelectedIndices[i] + ": " + LstData.SelectedItems[i] + "\r\n";
+            }
+        }
+
+        private void TxtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter && TxtInput.TextLength > 0)
+            {
+                if(LstData.Items.Contains(TxtInput.Text))
+                {
+                    MessageBox.Show("Frucht existiert schon!");
+                    return;
+                }
+
+                LstData.Items.Add(TxtInput.Text);
+                TxtInput.Clear();
+            }
+        }
+    }
+}
